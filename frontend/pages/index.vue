af54fbd82b318ab200782b584a0e3620ee48f3bd
unknown <you@example.com>
2025-07-18 11:22:15 +0700

    chau update UI

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,30 +1,31 @@
 <template>
-  <main class="bg-[#F5F5FA] py-2">
-    <div class="container mx-auto flex gap-4 px-4">
+  <main class="bg-[#f5f5f5] text-[#222222] mb-6">
+    <div class="max-w-[1200px] mx-auto px-2">
       <!-- Sidebar -->
-      <Sidebar />
+
       <!-- Main content -->
       <section class="flex-1 space-y-6">
         <!-- Banner -->
-        <Banner />      
+        <Banner />
         <!-- Brands -->
-        <Brands />
+        <categories />
+        <!-- // giảm giá -->
+        <Sale />
+
+        <ProductSearch />
         <!-- Products -->
         <Products />
-
-
-
-   
       </section>
     </div>
   </main>
 </template>
 
 <script setup>
-import Sidebar from '~/components/shared/layouts/Sidebar.vue'
-import Banner from '~/components/shared/Banner.vue'
-import Brands from '~/components/shared/Tags.vue'
-import Products from '~/components/shared/Products.vue'
-
-
+import Sidebar from "~/components/shared/layouts/Sidebar.vue";
+import categories from "~/components/shared/layouts/categories.vue";
+import Sale from "~/components/shared/Sale.vue";
+import ProductSearch from "~/components/shared/ProductSearch.vue";
+import Banner from "~/components/shared/Banner.vue";
+import Brands from "~/components/shared/Tags.vue";
+import Products from "~/components/shared/Products.vue";
 </script>
